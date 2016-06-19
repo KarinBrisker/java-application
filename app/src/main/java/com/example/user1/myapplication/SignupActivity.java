2347b168502178e9452f5ca99de9d66f19326805
LironGit <Lironshurgi@gmail.com>
2016-06-19 22:48:46 +0300

    added server for the sign in

--- app/src/main/java/com/example/user1/myapplication/SignupActivity.java
+++ app/src/main/java/com/example/user1/myapplication/SignupActivity.java
@@ -1,6 +1,7 @@
 package com.example.user1.myapplication;
 
 import android.app.ActionBar;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 
@@ -24,6 +25,14 @@ import butterknife.InjectView;
 import android.view.Gravity;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class SignupActivity extends ActionBarActivity {
@@ -171,6 +180,57 @@ public class SignupActivity extends ActionBarActivity {
         Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
 
         ok.setEnabled(true);
+    }
+
+
+    /***
+     * the server
+     */
+    public class ServerConMsg extends AsyncTask<Void, Void, String> {
+
+
+        ServerConMsg(){
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                String userStr = "UserName="+user_name;
+                String passStr = "&Password=" + password;
+                String realNameStr = "&RealName=" + nick_name;
+                String mailStr = "&Mail=" + email;
+                String iconStr = "&Img=" + icon;
+
+                //localhost:8080/ServerProj/ServerSign?UserName=galit&Password=1122&RealName=alma&Mail=galitgmailcom&Img=supergirlpng
+
+                String urlStr = "http://10.0.2.2:8080/ServerProj/ServerMsg?" +
+                        userStr+passStr+realNameStr+mailStr+iconStr;
+                URL url = new URL(urlStr);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                    BufferedReader streamReader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+                    StringBuilder responseStrBuilder = new StringBuilder();
+                    String getStr = streamReader.readLine();
+                    return getStr;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(String ansStr) {
+
+            //todo - "sign ok" - can continue
+            //       "sign error" - cant continue - try again
+        }
     }
 
 
